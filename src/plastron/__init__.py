659fba362f89b269ab41cf3248a5ce6225279616
tsifrer <3967564+tsifrer@users.noreply.github.com>
2025-10-16 16:28:44 +1300

    chore: another attempt at versions

--- src/plastron/__init__.py
+++ src/plastron/__init__.py
@@ -1,12 +1,3 @@
-from importlib.metadata import PackageNotFoundError, version
+from plastron.version import __version__
 
-try:
-    __version__ = version("plastron")
-except PackageNotFoundError:
-    import tomllib
-    from pathlib import Path
-
-    filepath = Path("./pyproject.toml")
-    with filepath.open("rb") as f:
-        pyproject = tomllib.load(f)
-    __version__ = pyproject["project"]["version"]
+__all__ = ["__version__"]
